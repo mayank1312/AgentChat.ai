ca89642b458e4968cf70ea457b89d51eeb51bac9
mayank1312 <mayankdhingra1312@gmail.com>
2025-07-04 16:40:53 +0530

    12:AgentsDataTable

---
+++ src/components/empty-state.tsx
@@ -0,0 +1,21 @@
+
+import { AlertCircleIcon } from "lucide-react";
+import Image from "next/image";
+
+interface Porps{
+    title:string,
+    description:string
+}
+
+export const EmptyState = ({title,description}:Porps) => {
+    return (
+        <div className="flex flex-col items-center justify-center py-4 px-8">
+            <Image src="/empty.svg" alt="Empty" width={240} height={240}/>
+            <div className="flex flex-col gap-y-6 max-w-md mx-auto text-center">
+                <h6 className="text-lg font-medium">{title}</h6>
+                <p className="text-sm text-muted-foreground">{description}</p>
+            </div>
+           
+        </div>
+    );
+}
