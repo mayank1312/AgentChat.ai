fabad6b249f4f399bff8d54a90443e9ea134f892
mayank1312 <mayankdhingra1312@gmail.com>
2025-08-09 00:51:50 +0530

    one more

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,53 +1,43 @@
 import { auth } from "@/lib/auth";
 import { MeetingsListHeaders } from "@/modules/meetings/ui/components/meetings-list-headers";
-import {
-  MeetingsView,
-  MeetingsViewError,
-  MeetingsViewLoading,
-} from "@/modules/meetings/ui/views/meetings-view";
-import { getQueryClient, trpc } from "@/trpc/server";
+import { MeetingsView, MeetingsViewError, MeetingsViewLoading } from "@/modules/meetings/ui/views/meetings-view"
+import { getQueryClient, trpc } from "@/trpc/server"
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import type { SearchParams } from "nuqs/server";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { loadSearchParams } from "@/modules/meetings/param";
 
-type PageProps = {
-  searchParams: Record<string, string | string[] | undefined>;
-};
-
-const Page = async ({ searchParams }: PageProps) => {
-  const filters = await loadSearchParams(searchParams);
-
-  const session = await auth.api.getSession({
-    headers: await headers(),
-  });
-
-  if (!session) {
-    redirect("/sign-in");
-  }
-
-  const queryClient = getQueryClient();
-
-  void queryClient.prefetchQuery(
-    trpc.meetings.getMany.queryOptions({
-      ...filters,
+interface Props{
+  searchParam:Promise<SearchParams>
+}
+const Page = async({searchParam}:Props) => {
+  const filters=await loadSearchParams(searchParam)
+   const session=await auth.api.getSession({
+          headers:await headers()
     })
-  );
-
+    
+    if(!session){
+      redirect("/sign-in")
+    }
+  const queryClient=getQueryClient();
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({
+    ...filters
+  }));
   return (
-    <>
-      <MeetingsListHeaders />
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<MeetingsViewLoading />}>
-          <ErrorBoundary fallback={<MeetingsViewError />}>
-            <MeetingsView />
-          </ErrorBoundary>
-        </Suspense>
-      </HydrationBoundary>
+<>
+<MeetingsListHeaders/>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingsViewLoading/>}>
+        <ErrorBoundary fallback={<MeetingsViewError/>}>
+     <MeetingsView/>
+     </ErrorBoundary>
+     </Suspense>
+    </HydrationBoundary>
     </>
-  );
-};
+  )
+}
 
-export default Page;
+export default Page
